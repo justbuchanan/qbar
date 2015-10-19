cab0fcb6f31c44e297830bdc9832c410ea749ab1
Justin Buchanan <justbuchanan@gmail.com>
2015-10-19 18:23:34 -0400

    updated configs

--- configs/primary_bottom.py
+++ configs/primary_bottom.py
@@ -2,8 +2,6 @@ from qbar.items import *
 
 items = [
     SpacerBarItem(),
-    DesktopTitleBarItem(monitor_index=1),
-    SpacerBarItem(),
     MemoryBarItem(),
     CpuBarItem(),
     MulticoreCpuBarItem(),
--- configs/secondary_bottom.py
+++ configs/secondary_bottom.py
@@ -2,6 +2,4 @@ from qbar.items import *
 
 items = [
     SpacerBarItem(),
-    DesktopTitleBarItem(),
-    SpacerBarItem(),
 ]
